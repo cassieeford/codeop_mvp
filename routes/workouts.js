0853e0b3ae018d329b3faead96f1efd24a1d44ec
Anna Chesson <ae.chesson@gmail.com>
2021-08-23 22:58:01 +0200

    can get a full workout object by id and return

--- routes/workouts.js
+++ routes/workouts.js
@@ -15,6 +15,42 @@ async function getWorkout(id) {
   return results.data.length === 1 ? results.data[0] : null;
 }
 
+//used to turn arrays of numbers into strings for sql statements
+function makeStr(arr) {
+  let str = '(';
+  for (let n in arr) {
+      if (n < arr.length -1) {
+          let s = String(arr[n]);
+          s = s + ', ';
+          str = str + s;
+      } else {
+          let s = String(arr[n]);
+           s = s + ')';
+          str = str + s;}
+  }
+  return str
+  }
+
+  function addExercisesToCircuits(circuitsArr, exercisesArr) {
+    //circuits.forEach(c => c.exercises = []);
+    for (let e in exercisesArr) {
+        let circuitIDforthisExercise = exercisesArr[e].circuitID;
+        //console.log(e.exerciseName, circuitIDforthisExercise)
+        for (let c in circuitsArr) {
+            if (circuitIDforthisExercise === circuitsArr[c].circuitID) {
+                //console.log('c', c);
+                //console.log('circuitsArr[c]', circuitsArr[c])
+                circuitsArr[c].exercises.push(exercisesArr[e]); 
+           
+        }
+    }
+}
+return circuitsArr;
+}
+
+
+
+
 // GET workouts list
 router.get("/", async function(req, res, next) {
   sendAllWorkouts(res);
@@ -31,11 +67,32 @@ router.get("/:id", async function(req, res, next) {
   WHERE workoutID = ${id}
   `;
   try {
-    let workout = await getWorkout(id);
-    if (workout) {
-      //return workout
+    //check if there is a workout by this id
+    let existsWorkout = await getWorkout(id);
+    if (existsWorkout) {
+      let workout = await db(`SELECT * FROM workouts where workoutID = ${id}`);
+      
+      //get circuits in this workout
+      let circuits = await db(`SELECT * FROM circuits where workoutID = ${id}`);
+      
+      //create an array with circuitID for each circuit
+      let circuitIDsArr = circuits.data.map(n=> n.circuitID)
+      //turn circuitIDsArr into a sql string e.g. [0,1] to '(0, 1)'
+      let circuitIDsArrSql = makeStr(circuitIDsArr);
+      //get exercises- select exercises where circuitID is in circuit ids
+      let exercises = await db(`SELECT * FROM exercises where circuitID IN ${circuitIDsArrSql}`);
+      //get the arrays of circuits and exercise objects 
+      let circuitsArr = circuits.data;
+      //needed for function
+      circuitsArr.forEach(c => c.exercises = []);
+      let exercisesArr = exercises.data;
+      let resultCircuits = addExercisesToCircuits(circuitsArr, exercisesArr);
+      let workoutdata = workout.data[0];
+      workoutdata.circuits = resultCircuits;
+
+
       res.status(200);
-      res.send(workout);
+      res.send(workoutdata); //workout
     } else {
       res.status(404).send({ error: "Workout not found" });
     }
